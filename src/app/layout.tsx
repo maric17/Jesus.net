70169b2abec8780c01058c2531408a91a831cada
Maric <maric@prosperna.com>
2026-03-21 10:10:32 +0800

    feat: Implement initial Next.js root layout with metadata, header, footer, and apple touch icon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,28 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Jesus.ph - Hope Begins Here",
-  description: "Free digital tools to help you know Jesus and share Him with others",
+  title: "Jesus.net - Free digital tools to help you know Jesus and share Him with others.",
+  description:
+    "Welcome to Jesus.net! Access mga articles, resources, at tools to deepen your faith and at mas makilala si Jesus—ang Kanyang mga teachings and love.",
+  openGraph: {
+    title: "Jesus.net - Free digital tools to help you know Jesus and share Him with others.",
+    description:
+      "Welcome to Jesus.net! Access mga articles, resources, at tools to deepen your faith and at mas makilala si Jesus—ang Kanyang mga teachings and love.",
+    url: "https://jesus.net",
+    siteName: "Jesus.net Philippines",
+    locale: "en_PH",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jesus.net - Free digital tools to help you know Jesus and share Him with others.",
+    description:
+      "Welcome to Jesus.net! Access mga articles, resources, at tools to deepen your faith and at mas makilala si Jesus—ang Kanyang mga teachings and love.",
+  },
+  icons: {
+    icon: "/apple-touch-icon.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
